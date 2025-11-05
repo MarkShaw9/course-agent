b05cf2d37edd5cc18afd7096910b45f432ed459d
Shaw <55754202+MarkShaw9@users.noreply.github.com>
2025-11-05 21:53:49 +1100

    Update base path in Vite configuration

--- course_agent/course_agent_frontend/vue-course/vite.config.js
+++ course_agent/course_agent_frontend/vue-course/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/course-agent',
+  base: '/course-agent/',
   plugins: [
     vue(),
     vueJsx(),
